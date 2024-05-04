1e035e6017ac11f69284d47c5476782139491597
Ashlynn "Void Voxel" Juniper <160202125+voidvoxel@users.noreply.github.com>
2024-05-04 18:07:01 -0400

    Add example `footer`

---
+++ examples/footer.mjs
@@ -0,0 +1,22 @@
+import { readFileSync } from 'fs';
+
+
+import { JavaScriptInjector } from "../JavaScriptInjector.mjs";
+
+
+const originalSourceCode = `console.log("Hello, world!");`;
+
+let sourceCode = originalSourceCode;
+
+let component = readFileSync(
+    "examples/components/hello-world.mjs",
+    'utf-8'
+);
+
+const injector = new JavaScriptInjector(sourceCode);
+
+injector.addFooter(component);
+
+const patchedSourceCode = sourceCode = injector.toString();
+
+eval(patchedSourceCode);
